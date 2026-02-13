6fd74bfc1986c5e9f55fdfcc38db08a462e2d046
sujeet <sujeetp2452@gmail.com>
2026-02-13 22:42:36 +0530

    Initial setup

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    win.loadFile('index.html');
+    // win.setMenuBarVisibility(false); // Uncomment to hide menu later
+}
+
+app.whenReady().then(createWindow);
